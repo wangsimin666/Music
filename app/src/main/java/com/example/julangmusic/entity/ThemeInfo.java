0ea8933dbbc234838972b263fe1a14f33b763639
Smallis_Service <smallis2018@gmail.com>
2020-06-16 15:07:16 +0800

    添加注释

--- app/src/main/java/com/example/julangmusic/entity/ThemeInfo.java
+++ app/src/main/java/com/example/julangmusic/entity/ThemeInfo.java
@@ -3,20 +3,38 @@ package com.example.julangmusic.entity;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+/**
+ *
+ * @ClassName:     ThemeInfo
+ * @Description:   主题实体类
+ * @Author:        ydl
+ * @date          2020/5/10
+ *
+ */
 public class ThemeInfo implements Parcelable {
+    public static final Creator<ThemeInfo> CREATOR = new Creator<ThemeInfo>() {
+        @Override
+        public ThemeInfo createFromParcel(Parcel source) {
+            return new ThemeInfo(source);
+        }
+
+        @Override
+        public ThemeInfo[] newArray(int size) {
+            return new ThemeInfo[size];
+        }
+    };
     private String name;
     private int color;
-//    private int background;
     private boolean isSelect;
 
-//
-//    public int getBackground() {
-//        return background;
-//    }
-//
-//    public void setBackground(int background) {
-//        this.background = background;
-//    }
+    public ThemeInfo() {
+    }
+
+    protected ThemeInfo(Parcel in) {
+        this.name = in.readString();
+        this.color = in.readInt();
+        this.isSelect = in.readByte() != 0;
+    }
 
     public String getName() {
         return name;
@@ -42,9 +60,6 @@ public class ThemeInfo implements Parcelable {
         isSelect = select;
     }
 
-    public ThemeInfo() {
-    }
-
     @Override
     public int describeContents() {
         return 0;
@@ -54,26 +69,6 @@ public class ThemeInfo implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.name);
         dest.writeInt(this.color);
-//        dest.writeInt(this.background);
         dest.writeByte(this.isSelect ? (byte) 1 : (byte) 0);
     }
-
-    protected ThemeInfo(Parcel in) {
-        this.name = in.readString();
-        this.color = in.readInt();
-//        this.background = in.readInt();
-        this.isSelect = in.readByte() != 0;
-    }
-
-    public static final Creator<ThemeInfo> CREATOR = new Creator<ThemeInfo>() {
-        @Override
-        public ThemeInfo createFromParcel(Parcel source) {
-            return new ThemeInfo(source);
-        }
-
-        @Override
-        public ThemeInfo[] newArray(int size) {
-            return new ThemeInfo[size];
-        }
-    };
 }
