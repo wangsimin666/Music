6befbe3b60440a7e7b2aa9a30fbe033f7a972165
Smallis_Service <smallis2018@gmail.com>
2020-06-16 15:12:03 +0800

    修复bug

--- app/src/main/java/com/example/julangmusic/util/SelectorUtil.java
+++ app/src/main/java/com/example/julangmusic/util/SelectorUtil.java
@@ -7,15 +7,23 @@ import android.graphics.drawable.StateListDrawable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+
+/**
+ *
+ * @ClassName:     SelectorUtil
+ * @Description:   selector工具
+ * @Author:        ydl
+ * @date          2020/5/11
+ *
+ */
 public class SelectorUtil {
     private static final String TAG = "SelectorUtil";
+
     /**
      * 动态修改selector中图片的背景颜色
      *
-     * @param drawable
-     *            selectorDrawable
-     * @param rgbColors
-     *            按下,默认的颜色值
+     * @param drawable  selectorDrawable
+     * @param rgbColors 按下,默认的颜色值
      */
     public static void changeViewColor(StateListDrawable drawable, int[] rgbColors) {
         Drawable.ConstantState cs = drawable.getConstantState();
@@ -45,11 +53,6 @@ public class SelectorUtil {
                     gd.setColor(rgbColors[1]);
                 }
             }
-            // 最后总结一下，为了实现这个效果，刚开始并没有看到setColor的方法，而是通过反射获取GradientDrawable对象的属性GradientState，
-            // 再通过反射调用GradientState对象的setSolidColor方法去实现，效果不太理想。
-            // 最后在仔仔细细一一看GradientDrawable对象的属性，发现属性Paint
-            // mFillPaint，从名字就可以看出这个对象是用来绘制drawable的背景的，
-            // 于是顺着往下找，发现setColor方法，于是bingo，这个过程也是挺曲折的。
 
         } catch (NoSuchMethodException e1) {
             e1.printStackTrace();
