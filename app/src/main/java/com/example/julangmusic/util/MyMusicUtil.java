ae93949971cd6553c0464dc75a6088f68f6d6e01
wangwang66 <603281257@qq.com>
2020-06-19 23:20:54 +0800

    添加注释

--- app/src/main/java/com/example/julangmusic/util/MyMusicUtil.java
+++ app/src/main/java/com/example/julangmusic/util/MyMusicUtil.java
@@ -145,27 +145,6 @@ public class MyMusicUtil {
         dbManager.setMyLove(musicId);
     }
 
-    //设置--铃声的具体方法
-    public static void setMyRingtone(Context context) {
-        DBManager dbManager = DBManager.getInstance(context);
-        int musicId = MyMusicUtil.getIntShared(Constant.KEY_ID);
-        String path = dbManager.getMusicPath(musicId);
-        File sdfile = new File(path);
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.MediaColumns.DATA, sdfile.getAbsolutePath());
-        values.put(MediaStore.MediaColumns.TITLE, sdfile.getName());
-        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/*");
-        values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
-        values.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);
-        values.put(MediaStore.Audio.Media.IS_ALARM, false);
-        values.put(MediaStore.Audio.Media.IS_MUSIC, false);
-
-        Uri uri = MediaStore.Audio.Media.getContentUriForPath(sdfile.getAbsolutePath());
-        Uri newUri = context.getContentResolver().insert(uri, values);
-        RingtoneManager.setActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE, newUri);
-        Toast.makeText(context, "设置来电铃声成功！", Toast.LENGTH_SHORT).show();
-    }
-
     // 设置sharedPreferences
     public static void setShared(String key, int value) {
         SharedPreferences pref = MyApplication.getContext().getSharedPreferences("music", MyApplication.getContext().MODE_PRIVATE);
